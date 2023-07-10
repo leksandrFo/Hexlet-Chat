6d5111fff8c4c8b4d95700c45f093ab101d2f44f
leksandrFo <leksandr.fo@gmail.com>
2023-07-10 20:52:59 +0300

    Added Rollbar (react)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 import leoProfanity from 'leo-profanity';
 import { ToastContainer } from 'react-toastify';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 import AuthProvider from './providers/AuthProvider.jsx';
 import SocketProvider from './providers/SocketProvider.jsx';
 import I18nProvider from './providers/I18nextProvider.jsx';
@@ -18,6 +19,15 @@ import ChatPage from './components/pages/ChatPage.jsx';
 import Nav from './components/Nav.jsx';
 import { appRoutes } from './routes/routes.js';
 import { useAuth } from './hooks/index.jsx';
+
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
+};
 
 const PrivateRoute = ({ children }) => {
   const auth = useAuth();
@@ -32,29 +42,33 @@ leoProfanity.add(leoProfanity.getDictionary('ru'), leoProfanity.getDictionary('e
 
 const App = () => (
   <div className="d-flex flex-column h-100">
-    <I18nProvider>
-      <AuthProvider>
-        <SocketProvider>
-          <Nav />
-          <BrowserRouter>
-            <Routes>
-              <Route
-                path={appRoutes.chatPage()}
-                element={(
-                  <PrivateRoute>
-                    <ChatPage />
-                  </PrivateRoute>
-                )}
-              />
-              <Route path={appRoutes.loginPage()} element={<LoginPage />} />
-              <Route path={appRoutes.registrationPage()} element={<RegistrationPage />} />
-              <Route path={appRoutes.notFoundPage()} element={<NotFoundPage />} />
-            </Routes>
-          </BrowserRouter>
-          <ToastContainer />
-        </SocketProvider>
-      </AuthProvider>
-    </I18nProvider>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <I18nProvider>
+          <AuthProvider>
+            <SocketProvider>
+              <Nav />
+              <BrowserRouter>
+                <Routes>
+                  <Route
+                    path={appRoutes.chatPage()}
+                    element={(
+                      <PrivateRoute>
+                        <ChatPage />
+                      </PrivateRoute>
+                    )}
+                  />
+                  <Route path={appRoutes.loginPage()} element={<LoginPage />} />
+                  <Route path={appRoutes.registrationPage()} element={<RegistrationPage />} />
+                  <Route path={appRoutes.notFoundPage()} element={<NotFoundPage />} />
+                </Routes>
+              </BrowserRouter>
+              <ToastContainer />
+            </SocketProvider>
+          </AuthProvider>
+        </I18nProvider>
+      </ErrorBoundary>
+    </Provider>
   </div>
 );
 
